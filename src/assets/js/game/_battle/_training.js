aab991ef424d3e49d6ea08f36b2a79be87051d94
Wheels <leroux.70.13@hotmail.fr>
2021-03-25 21:53:27 +0100

    Gestion Menu Display en TRAINING

--- src/assets/js/game/_battle/_training.js
+++ src/assets/js/game/_battle/_training.js
@@ -1,5 +1,5 @@
-/* ----- BattleTraining ----- */
-function BattleTraining(aPlayer){
+/* ----- BattleTrainingDisplay ----- */
+function BattleTrainingDisplay(oContext, aPlayer){
     this.aPlayer = null;
     
     this.bInit = false;
@@ -7,11 +7,17 @@ function BattleTraining(aPlayer){
     this.aHistory = [];
     this.aAnimation = [];
 
-    this.init(aPlayer);
+    this.oShow = {
+        bHistory: false,
+        bBox: false,
+        bAnimation: false
+    };
+
+    this.init(oContext, aPlayer);
 }
 
 Object.assign(
-    BattleTraining, {
+    BattleTrainingDisplay, {
 
         oSymbolHistory: {
             oNormal: {
@@ -45,7 +51,8 @@ Object.assign(
         },
 
         prototype: {
-            init: function(aPlayer){
+            init: function(oContext, aPlayer){
+                this.oContext = oContext;
                 this.aPlayer = aPlayer;
 
                 aPlayer.forEach( oPlayer => {
@@ -63,77 +70,127 @@ Object.assign(
             },
             update: function(){
                 this.aPlayer.forEach( (oPlayer, nIndex) => {
+
                     // History
-                    oPlayer.oInputBuffer.aHistory.forEach( (oHistory, nHistory, aHistory) => {
-                        const aBtn = Object.keys( oHistory.oButtons ),
-                            oBtn = { A: true, B: true, C: true },
-                            oSymbol = BattleTraining.oSymbolHistory[ oPlayer.nPlayer == 1 ? 'oNormal' : 'oReverse' ];
-
-                        let oTextHist = this.aHistory[nIndex].aChildElement[nHistory],
-                            sText = '',
-                            sFrame = '';
-
-                        nIndex || aBtn.unshift( aBtn.pop() );
-                        aBtn.forEach( sBtn => {
-                            if( oSymbol[sBtn] ){
-                                sText += '<b class="' + ( oBtn[sBtn] ? '--btn' : '--dir' ) +  '">' + oSymbol[sBtn] + '</b>';
+                    if( this.oShow.bHistory ){
+                        oPlayer.oInputBuffer.aHistory.forEach( (oHistory, nHistory, aHistory) => {
+                            const aBtn = Object.keys( oHistory.oButtons ),
+                                oBtn = { A: true, B: true, C: true },
+                                oSymbol = BattleTrainingDisplay.oSymbolHistory[ oPlayer.nPlayer == 1 ? 'oNormal' : 'oReverse' ];
+                                
+                                let oTextHist = this.aHistory[nIndex].aChildElement[nHistory],
+                                sText = '',
+                                sFrame = '';
+                                
+                                nIndex || aBtn.unshift( aBtn.pop() );
+                                aBtn.forEach( sBtn => {
+                                    if( oSymbol[sBtn] ){
+                                        sText += '<b class="' + ( oBtn[sBtn] ? '--btn' : '--dir' ) +  '">' + oSymbol[sBtn] + '</b>';
+                                    }
+                                } );
+                                
+                                if( nHistory == aHistory.length - 1 ){
+                                    sFrame = '<i>' + ( GAME.oTimer.nFrames - oHistory.nFrame + 1 ) + '</i>';
+                                } else {
+                                    sFrame = '<i>' + ( aHistory[ nHistory + 1 ].nFrame - oHistory.nFrame ) + '</i>';
+                            }
+                            nIndex ? (sText = sFrame + sText) : (sText += sFrame);
+                            
+                            if( oTextHist ){
+                                oTextHist.setText(sText);
+                            } else {
+                                oTextHist = new GAME.oOutput.OutputText(sText);
+                                this.aHistory[nIndex].add(oTextHist);
                             }
                         } );
-
-                        if( nHistory == aHistory.length - 1 ){
-                            sFrame = '<i>' + ( GAME.oTimer.nFrames - oHistory.nFrame + 1 ) + '</i>';
-                        } else {
-                            sFrame = '<i>' + ( aHistory[ nHistory + 1 ].nFrame - oHistory.nFrame ) + '</i>';
-                        }
-                        nIndex ? (sText = sFrame + sText) : (sText += sFrame);
-
-                        if( oTextHist ){
-                            oTextHist.setText(sText);
-                        } else {
-                            oTextHist = new GAME.oOutput.OutputText(sText);
-                            this.aHistory[nIndex].add(oTextHist);
-                        }
-                    } );
+                    }
 
                     // Box
-                    ['oPositionBox', 'oHurtBox', 'oHitBox'].forEach( sBox => {
-                        const oBox = oPlayer.getCharacterBox(sBox)
-                        if( oBox ){
-                            this.aBox[nIndex][sBox].setStyle( {
-                                display: null,
-                                left: ( GAME.oSettings.oPositionPoint.nX + oBox.nX ) + 'px',
-                                top: ( GAME.oSettings.oPositionPoint.nY + oBox.nY ) + 'px',
-                                width: oBox.nWidth + 'px',
-                                height: oBox.nHeight + 'px'
-                            } );
-                        } else {
-                            this.aBox[nIndex][sBox].setStyle( { display: 'none' } );
-                        }
-                    } );
+                    if( this.oShow.bBox ){
+                        ['oPositionBox', 'oHurtBox', 'oHitBox'].forEach( sBox => {
+                            const oBox = oPlayer.getCharacterBox(sBox)
+                            if( oBox ){
+                                this.aBox[nIndex][sBox].setStyle( {
+                                    display: null,
+                                    left: ( GAME.oSettings.oPositionPoint.nX + oBox.nX ) + 'px',
+                                    top: ( GAME.oSettings.oPositionPoint.nY + oBox.nY ) + 'px',
+                                    width: oBox.nWidth + 'px',
+                                    height: oBox.nHeight + 'px'
+                                } );
+                            } else {
+                                this.aBox[nIndex][sBox].setStyle( { display: 'none' } );
+                            }
+                        } );
+                    }
 
                     // Animation
-                    const oAnimation = this.aAnimation[nIndex];
-                    if( oPlayer.oAnimation.sType != 'movement' ){
-                        if( oPlayer.oAnimation != oAnimation.oLast ){
-                            oAnimation.oLast = oPlayer.oAnimation;
-                            oAnimation.oLayer.hElement.innerHTML = '';
+                    if( this.oShow.bAnimation ){
+                        const oAnimation = this.aAnimation[nIndex];
+                        if( oPlayer.oAnimation.sType != 'movement' ){
+                            if( oPlayer.oAnimation != oAnimation.oLast ){
+                                oAnimation.oLast = oPlayer.oAnimation;
+                                oAnimation.oLayer.hElement.innerHTML = '';
+                            }
+                            let sClass = '--' + oPlayer.oAnimation.sType;
+                            if( oPlayer.oAnimation.oFrame.bFreeze ){
+                                sClass = '--freeze';
+                            } else if( oPlayer.oAnimation.oFrame.oStatus.bGuard ){
+                                sClass = '--guard';
+                            } else if( oPlayer.oAnimation.oFrame.oHitBox ){
+                                sClass = '--damage';
+                            } else if( !oPlayer.oAnimation.oFrame.oHurtBox ){
+                                sClass = '--invulnerable';
+                            }
+                            oAnimation.oLayer.hElement.innerHTML += '<span class="' + sClass + '"></span>'
                         }
-                        let sClass = '--' + oPlayer.oAnimation.sType;
-                        if( oPlayer.oAnimation.oFrame.bFreeze ){
-                            sClass = '--freeze';
-                        } else if( oPlayer.oAnimation.oFrame.oStatus.bGuard ){
-                            sClass = '--guard';
-                        } else if( oPlayer.oAnimation.oFrame.oHitBox ){
-                            sClass = '--damage';
-                        } else if( !oPlayer.oAnimation.oFrame.oHurtBox ){
-                            sClass = '--invulnerable';
-                        }
-                        oAnimation.oLayer.hElement.innerHTML += '<span class="' + sClass + '"></span>'
                     }
                 } );
             },
             destroy: function(){
+                for( let sType in this.oShow ){
+                    this.hide(sType);
+                }
+                this.oContext.update();
+            },
 
+            show: function(sType){
+                this.oShow[sType] = true;
+                this.oContext.addTickUpdate( () => {
+                    this.oContext.hElement.classList.add('--' + sType);
+                } );
+            },
+            hide: function(sType){
+                this.oShow[sType] = false;
+                this.oContext.addTickUpdate( () => {
+                    this.oContext.hElement.classList.remove('--' + sType);
+                } );
+            },
+            toogle: function(sType){
+                this[ this.oShow[sType] ? 'hide' : 'show' ](sType);
+            }
+        }
+    }
+);
+
+/* ----- BattleTraining ----- */
+function BattleTraining(oContext, aPlayer){
+    this.oDisplay = null;
+
+    this.init(oContext, aPlayer);
+}
+
+Object.assign(
+    BattleTraining, {
+        prototype: {
+            constructor: BattleTraining,
+            init: function(oContext, aPlayer){
+                this.oDisplay = new BattleTrainingDisplay(oContext, aPlayer);
+            },
+            update: function(){
+                this.oDisplay.update();
+            },
+            destroy: function(){
+                this.oDisplay.destroy();
             }
         }
     }
--- src/assets/js/game/_scene/_training.js
+++ src/assets/js/game/_scene/_training.js
@@ -26,41 +26,23 @@ Object.assign(
                         }
                     );
 
-                    this.oTraining = new BattleTraining( this.aPlayer );
+                    this.oTraining = new BattleTraining( this.oContext, this.aPlayer );
                     this.oMenu = {
-                        oPrimary: new GameMenu('LAY__Training_Menu_Primary')
+                        oPrincipal: new GameMenu('LAY__Training_Menu_Principal'),
+                        // oParameters: null,
+                        oDisplay: new GameMenu('LAY__Training_Menu_Display')
                     };
-                    this.oCurrentMenu = this.oMenu.oPrimary
                 },
                 update: function(){
                     this.oKeyboard.ifPressedNow( {
                         START: () => {
-                            this.toogleMenu();
+                            this.toogleMenuPrincipal();
                         }
                     } );
 
                     if( this.bMenu ){
-                        this.oKeyboard.ifPressedNow( {
-                            // Gestion validation
-                            A: () => {
-                                let oMenuSelected = this.oCurrentMenu.getSelected();
-                                switch( oMenuSelected.sId ){
-                                    case 'TXT__Training_Menu_Continue':
-                                        this.toogleMenu();
-                                        break;
-                                    case 'TXT__Training_Menu_Quit':
-                                        GAME.oScene.change( new MenuScene() );
-                                        break;
-                                }
-                            },
-                            // Gestion déplacement
-                            UP: () => {
-                                this.oCurrentMenu.prev();
-                            },
-                            DOWN: () => {
-                                this.oCurrentMenu.next();
-                            }
-                        } );
+                        this.controlsMenu();
+                        this.updateMenu();
                         this.oCurrentMenu.update();
                     } else {
                         BattleScene.prototype.update.call(this);
@@ -68,18 +50,89 @@ Object.assign(
                     }
                 },
                 destroy: function(){
-                    this.oContext.hElement.classList.remove('--menu');
-                    this.oMenu.oPrimary.destroy();
+                    this.oTraining.destroy();
+                    this.toogleMenuPrincipal(false);
+                    for( let sMenu in this.oMenu ){
+                        this.oMenu[sMenu].destroy();
+                    }
                 },
 
-                toogleMenu: function(){
+                toogleMenuPrincipal: function(bMenu){
+                    this.bMenu = bMenu == null ? !this.bMenu : bMenu;
+                    this.switchMenu('oPrincipal');
                     this.aPlayer.forEach( oPlayer => {
                         oPlayer.oAnimation[this.bMenu ? 'unFreeze' : 'setFreeze']();
                     } );
                     this.oContext.addTickUpdate( () => {
                         this.oContext.hElement.classList[this.bMenu ? 'add' : 'remove']('--menu');
                     } );
-                    this.bMenu = !this.bMenu;
+                },
+                switchMenu: function(sMenu){
+                    const oLast = this.oCurrentMenu;
+                    this.oCurrentMenu = this.oMenu[sMenu];
+                    this.oContext.addTickUpdate( () => {
+                        oLast && oLast.oLayer.hElement.classList.remove('--show');
+                        this.oCurrentMenu.oLayer.hElement.classList.add('--show');
+                    } );
+                },
+                controlsMenu: function(){
+                    this.oKeyboard.ifPressedNow( {
+                        // Gestion validation
+                        A: () => {
+                            let oMenuSelected = this.oCurrentMenu.getSelected();
+                            switch( oMenuSelected.sId ){
+                                // Principal
+                                case 'TXT__Training_Menu_Display':
+                                    this.switchMenu('oDisplay');
+                                    break;
+                                case 'TXT__Training_Menu_Continue':
+                                    this.toogleMenuPrincipal();
+                                    break;
+                                case 'TXT__Training_Menu_Quit':
+                                    GAME.oScene.change( new MenuScene() );
+                                    break;
+
+                                // Display
+                                case 'LAY__Training_Menu_Display_Input':
+                                    this.oTraining.oDisplay.toogle('bHistory');
+                                    break;
+                                case 'LAY__Training_Menu_Display_Box':
+                                    this.oTraining.oDisplay.toogle('bBox');
+                                    break;
+                                case 'LAY__Training_Menu_Display_Animations':
+                                    this.oTraining.oDisplay.toogle('bAnimation');
+                                    break;
+                                case 'LAY__Training_Menu_Display_Return':
+                                    this.switchMenu('oPrincipal');
+                                    break;
+                            }
+                        },
+                        // Gestion déplacement
+                        UP: () => {
+                            this.oCurrentMenu.prev();
+                        },
+                        DOWN: () => {
+                            this.oCurrentMenu.next();
+                        }
+                    } );
+                },
+                updateMenu: function(){
+                    const oType = {
+                        bHistory: 'LAY__Training_Menu_Display_Input',
+                        bBox: 'LAY__Training_Menu_Display_Box',
+                        bAnimation: 'LAY__Training_Menu_Display_Animations'
+                    };
+                    switch( this.oCurrentMenu.oLayer.sId ){
+                        case 'LAY__Training_Menu_Display':
+                            for( let sType in oType ){
+                                GAME.oOutput.getElement(oType[sType]).aChildElement[0].setText(
+                                    this.oTraining.oDisplay.oShow[sType] ?
+                                        'Show' : 
+                                        'Hide'
+                                );
+                            }
+                            break;
+                    }
                 }
             }
         )
